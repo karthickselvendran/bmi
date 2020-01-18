c101c382f7aa1758a0ea6131ebbf4b4c6c648e73
Karthick S <selvendrankarthick@gmail.com>
2020-01-18 15:16:22 +0530

    Updates

---
+++ precache-manifest.e8087366681dbbbc352ceca3164d6231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30c03a723aa4ffc113477b7d31964dd",
+    "url": "/bmi/index.html"
+  },
+  {
+    "revision": "94b36dd740545c88443c",
+    "url": "/bmi/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "24f53610533c8c155cac",
+    "url": "/bmi/static/js/2.2bac8e10.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/bmi/static/js/2.2bac8e10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94b36dd740545c88443c",
+    "url": "/bmi/static/js/main.f5c1748f.chunk.js"
+  },
+  {
+    "revision": "0f99a61d38c3675cda84",
+    "url": "/bmi/static/js/runtime-main.e8ca2f0a.js"
+  }
+]);
